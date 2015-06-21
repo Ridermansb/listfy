e2d302bb23f8582e84fd8338d69dda946c9e540a
ridermansb <ridermansb@gmail.com>
2015-06-21 14:16:58 -0300

    Fix missing depenedency in karma configuration for ngCordova

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,8 @@ module.exports = function (config) {
 
         files: [
             'www/assets/lib/ionic/js/ionic.bundle.js',
+            'www/assets/lib/ngCordova/dist/ng-cordova.js',
+            'www/assets/lib/ionic-service-core/ionic-core.js',
             'www/assets/lib/angular-mocks/angular-mocks.js',
             'www/app/app.js',
             'www/app/components/**/*Controller.js',
