3dce51d3191cc78456a32b86ea9ab7a80f7e58a2
ridermansb <ridermansb@gmail.com>
2015-06-20 21:01:31 -0300

    Config tests and add bootstrap controller
     karma and wallaby configured;
     Add homeController with same tests

---
+++ README.md
@@ -0,0 +1,1 @@
+## File Structure from: https://scotch.io/tutorials/angularjs-best-practices-directory-structure
---
+++ wallaby-config.js
@@ -0,0 +1,29 @@
+module.exports = function () {
+    return {
+        testFramework: 'mocha@2.0.1',
+
+        files: [
+            {pattern: 'node_modules/chai/chai.js', instrument: false},
+            'www/assets/lib/ionic/js/ionic.bundle.js',
+            'www/assets/lib/angular-mocks/angular-mocks.js',
+            'www/app/app.js',
+            'www/app/components/**/*Controller.js',
+            'www/app/components/**/*Service.js',
+        ],
+
+        tests: [
+            'tests/**/*Test.js'
+        ],
+
+        bootstrap: function (wallaby) {
+            wallaby.delayStart();
+
+            var mocha = wallaby.testFramework;
+            mocha.ui('bdd');
+
+            window.assert = chai.assert;
+            window.expect = chai.expect;
+            wallaby.start();
+        }
+    };
+};
